3d8b0111708e599a04295f6f7f02c9e74d8646a4
ahfateme79 <ahfateme79@gmail.com>
2021-10-27 11:15:18 -0700

    login

---
+++ js/password.js
@@ -0,0 +1,14 @@
+var password = document.getElementById('fakePassword');
+      var toggler = document.getElementById('toggler');
+
+      showHidePassword = () => {
+        if (password.type == 'password') {
+          password.setAttribute('type', 'text');
+          toggler.innerHTML="hide";
+        } else {
+            toggler.innerHTML="show";
+          password.setAttribute('type', 'password');
+        }
+      };
+
+      toggler.addEventListener('click', showHidePassword);
