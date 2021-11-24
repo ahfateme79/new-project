4f17c759079ccb23a4d6d83c57fa41bc2b652cf0
ahfateme79 <ahfateme79@gmail.com>
2021-11-24 06:53:51 -0800

    commit

--- js/script.js
+++ js/script.js
@@ -18,45 +18,43 @@ var swiper = new Swiper('.swiper', {
 // change style of elemnts when scroll
 let links = document.querySelectorAll("nav ul li a")
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 500) {
+    if (window.scrollY < 500) {
+        document.querySelector("header").style.background = "#1f1e28";
+        document.documentElement.style.setProperty('--color', "#fff");
+        document.querySelector('.nav-menu').style.background = "#1f1e28";
+        // document.querySelector('.nav-menu ul li a').style.color = "#fff"
+        document.querySelector('.menu-title').style.color = "#fff";
+        document.querySelector('.name').style.color = "#fff";
+        // document.querySelector('.logo-icon').src = '../img/WnyXWTp.png'
+        // document.querySelector('.logo').style.color = 'white';
+        document.querySelector('.login').style.color = '#fff';
+        document.querySelector('.login-mobile').style.color = '#fff';
+        document.querySelector('.toggle-wrap').style.color = '#fff';
+        document.querySelector('.nav-menu').style.borderColor = "#2d3846";
+
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.color = '#fff'
+        }
+    } else if (window.scrollY > 500) {
         document.querySelector("header").style.background = "rgb(240, 236, 233)";
         // document.querySelector('.logo-icon').src = '../img/sololearn.png'
-        document.documentElement.style.setProperty('--color', "black")
-        document.querySelector('.logo').style.color = 'black'
-        document.querySelector('.login').style.color = '#2493df'
-        document.querySelector('.login-mobile').style.color = '#2493df'
-        document.querySelector('.nav-menu').style.background = "#fff"
-        document.querySelector('.nav-menu').style.borderColor = "transparent"
-        document.querySelector('.nav-menu ul').style.borderColor = "#bcd0e9"
-        document.querySelector('.nav-menu ul li a').style.color = "#1f1e28"
-        document.querySelector('nav ul li a').style.color = "#1f1e28"
-        document.querySelector('.menu-title').style.color = "#1f1e28"
-        document.querySelector('.name').style.color = "#1f1e28"
+        document.documentElement.style.setProperty('--color', "black");
+        // document.querySelector('.logo').style.color = 'black'
+        document.querySelector('.login').style.color = '#2493df';
+        document.querySelector('.login-mobile').style.color = '#2493df';
+        document.querySelector('.nav-menu').style.background = "#fff";
+        document.querySelector('.nav-menu').style.borderColor = "transparent";
+        document.querySelector('.nav-menu ul').style.borderColor = "#bcd0e9";
+        // document.querySelector('.nav-menu ul li a').style.color = "#1f1e28"
+        // document.querySelector('nav ul li a').style.color = "#1f1e28"
+        document.querySelector('.menu-title').style.color = "#1f1e28";
+        document.querySelector('.name').style.color = "#1f1e28";
 
 
         document.querySelector('.toggle-wrap').style.color = 'black'
 
         for (let i = 0; i < links.length; i++) {
             links[i].style.color = 'black'
-        }
-    } else if (window.scrollY < 500) {
-        document.querySelector("header").style.background = "#1f1e28";
-        document.documentElement.style.setProperty('--color', "#fff")
-        document.querySelector('.nav-menu').style.background = "#1f1e28"
-        document.querySelector('.nav-menu ul li a').style.color = "#fff"
-        document.querySelector('.menu-title').style.color = "#fff"
-        document.querySelector('.name').style.color = "#fff"
-
-
-        // document.querySelector('.logo-icon').src = '../img/WnyXWTp.png'
-        document.querySelector('.logo').style.color = '#fff'
-        document.querySelector('.login').style.color = '#fff'
-        document.querySelector('.login-mobile').style.color = '#fff'
-        document.querySelector('.toggle-wrap').style.color = '#fff'
-        document.querySelector('.nav-menu').style.borderColor = "#2d3846"
-
-        for (let i = 0; i < links.length; i++) {
-            links[i].style.color = '#c8d2db'
         }
     }
 });
